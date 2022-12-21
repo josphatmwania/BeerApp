f1534abf92053c18e3221803a15d3c5257c1639a
j23747 <josphatmwania237@gmail.com>
2022-12-21 13:52:26 +0300

    Add Beer API Interface

--- app/src/main/java/com/example/beerapp/network/BeerApiService.kt
+++ app/src/main/java/com/example/beerapp/network/BeerApiService.kt
@@ -2,12 +2,29 @@ package com.example.beerapp.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
 private const val BASE_URL = "https://api.punkapi.com/v2/beers/random"
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
+
+
+/**
+ * This interface defines who Retrofit talks to the web server using HTTP requests
+ *
+ */
+
+
+interface BeerApiService {
+
+    @ GET("beers")
+    fun getBeers(
+
+    )
+
+}
 
 
 
