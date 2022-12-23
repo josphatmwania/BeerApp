be1ddd7b4f91936f72b7bdd3b99e7f90cbf160b8
j23747 <josphatmwania237@gmail.com>
2022-12-23 14:18:41 +0300

    use Moshi to convert the JSON response into Kotlin objects

--- app/src/main/java/com/example/beerapp/network/BeerApiService.kt
+++ app/src/main/java/com/example/beerapp/network/BeerApiService.kt
@@ -3,6 +3,15 @@ package com.example.beerapp.network
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
+import com.squareup.moshi.Moshi
+//import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+
+/**
+ * use Moshi to convert the JSON response into Kotlin objects
+ */
+
+private val moshi = Moshi.Builder()
 
 private const val BASE_URL = "https://api.punkapi.com/v2/beers/random"
 private val retrofit = Retrofit.Builder()
