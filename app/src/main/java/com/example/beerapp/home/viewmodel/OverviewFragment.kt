3cab65bf3090dc0888527a6002a88f5d8332bb1a
j23747 <josphatmwania237@gmail.com>
2022-12-26 17:24:22 +0300

    Add Databinding Depedencies

--- app/src/main/java/com/example/beerapp/home/viewmodel/OverviewFragment.kt
+++ app/src/main/java/com/example/beerapp/home/viewmodel/OverviewFragment.kt
@@ -1,6 +1,12 @@
 package com.example.beerapp.home.viewmodel
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewmodels
+import com.example.beerapp.databinding.FragmentOVerviewBinding
 
 
 /**
@@ -10,6 +16,30 @@ import androidx.fragment.app.Fragment
 class OverviewFragment : Fragment() {
 
     private val viewModel: OverviewViewModel by viewModels()
+
+
+    /**
+     * This OncreateView fun Inflates the layout with Data Binding, sets its lifecycle owner to the OverviewFragment
+     * to enable Data Binding to observe LiveData, and sets up the RecyclerView with an adapter.
+    */
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentOverviewBinding.inflater(inflater)
+
+        /**
+         * Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+         */
+
+        binding.lifecycleOwner = this
+
+        binding.viewmodel = viewModel
+
+        return binding.root
+
+    }
 
 
 }
