4d956a12fe62aeed3b76f207c33bbfdef80563dc
j23747 <josphatmwania237@gmail.com>
2023-01-10 02:14:40 +0300

    Modify the BindingAdapter

--- app/src/main/java/com/example/beerapp/home/ui/BindingAdapters.kt
+++ app/src/main/java/com/example/beerapp/home/ui/BindingAdapters.kt
@@ -1,10 +1,22 @@
 package com.example.beerapp.home.ui
 
 import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import coil.load
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
+
+    /**
+     * convert the URL string to a Uri object using the toUri() method.
+
+     */
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        imgView.load(imgUri)
+
+    }
 
 }
 class BindingAdapters {
