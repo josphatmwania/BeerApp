63bb2398bf852fbdfe73e722d477a7d5018b354c
j23747 <josphatmwania237@gmail.com>
2023-01-10 01:16:57 +0300

    Add Kapt plugin

--- app/src/main/java/com/example/beerapp/home/ui/BindingAdapters.kt
+++ app/src/main/java/com/example/beerapp/home/ui/BindingAdapters.kt
@@ -1,8 +1,9 @@
 package com.example.beerapp.home.ui
 
 import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 
-
+@BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
 
 }
