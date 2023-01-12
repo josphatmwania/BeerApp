e7523252ec9c94d61c81b1c0d489cb9c632a80cf
MamboBryan <mambobryan@gmail.com>
2023-01-12 08:52:27 +0300

    adds todo to change image json name
    I've updated the BeerImages.kt file and left a todo check it out

--- app/src/main/java/com/example/beerapp/network/BeerImages.kt
+++ app/src/main/java/com/example/beerapp/network/BeerImages.kt
@@ -4,5 +4,6 @@ import com.squareup.moshi.Json
 
 data class BeerImages (
     val id: String,
+    // TODO: change this line to @Json(name = "image_url) val imgSrcUrl : String
     @Json(name= "img_src") val imgSrcUrl: String
         )
